9397a2d3ca957858a563d0999e8d86ffd2b4aca4
samuel-cavalcanti <scavalcanti111@gmail.com>
2024-02-01 11:34:48 -0300

    updated github repo

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 #
 #
 rm -rf _site
-JEKYLL_ENV=production bundle exec jekyll b -d _site/test_chirpy;
+JEKYLL_ENV=production bundle exec jekyll b -d _site/;
 
 bundle exec htmlproofer _site --disable-external=true --ignore-urls "http://127.0.0.1, http://0.0.0.0, http://localhost"
